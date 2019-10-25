e947a2730f4e7fec50ba4a56580bf99c175c8c6a
luomurong <coolpad@yulong.com>
2019-10-25 11:49:50 +0800

    Update model

--- Models/search_words.py
+++ Models/search_words.py
@@ -46,10 +46,10 @@ class SearchWords:
         self.ConversionShare3 = json_data.get('#3 Conversion Share')
 
     def convert_to_float(self, value):
+        if isinstance(value, str):
+            return 0.0
         if not isinstance(value, float):
             return float(value)
-        elif isinstance(value, str):
-            return 0.0
         else:
             return value
 
