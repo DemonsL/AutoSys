7d81037a1a0a30d776bb4d12233410aeff190977
luomurong <coolpad@yulong.com>
2019-10-25 18:23:32 +0800

    Update csv file

--- Autos/excel_to_sql.py
+++ Autos/excel_to_sql.py
@@ -1,29 +1,81 @@
 # coding: utf-8
+import os, sys, shutil
+sys.path.append('../')
 import json
+import logging
+import datetime
 import pandas as pd
 from Models import bussiness
+from Models import search_words
+
+
+formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+file_name = '/home/develop/logs/excel_to_sql/{}.log'.format(datetime.date.today())
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+
+fh = logging.FileHandler(file_name, mode='a+')
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+log.addHandler(fh)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+ch.setFormatter(formatter)
+log.addHandler(ch)
 
 
 class ExcelToSql:
 
-    def add_to_sql(self, snap_date, country, json_data):
-        session = bussiness.DBSession()
-        for data in json_data:
-            data_to_sql = bussiness.AscAsinBussiness(snap_date, country, data)
-            session.add(data_to_sql)
-        session.commit()
+    def add_to_sql(self, tb_name, json_data, snap_date=None, country=None):
+        if tb_name == 'AscAsinBussiness':
+            tb_excute = 'bussiness.{}'.format(tb_name)
+            b_session = bussiness.DBSession()
+            for data in json_data:
+                data_to_sql = eval(tb_excute)(snap_date, country, data)
+                b_session.add(data_to_sql)
+            b_session.commit()
 
-    def csv_to_json(self, file_name):
-        csv_data = pd.read_csv(file_name, encoding='utf-8')
-        json_data = csv_data.to_json(orient='records')
+        if tb_name in ['AscSearchWeek', 'AscSearchMonth']:
+            tb_excute = 'search_words.{}'.format(tb_name)
+            s_session = search_words.DBSession()
+            for data in json_data:
+                data_to_sql = eval(tb_excute)(data)
+                s_session.add(data_to_sql)
+            s_session.commit()
+
+    def data_to_json(self, file_name):
+        f_format = file_name.split('.')[1]
+        data = ''
+        if f_format == 'csv':
+            data = pd.read_csv(file_name, encoding='utf-8')
+        if f_format == 'xlsx':
+            # d_keys = pd.read_excel(file_name, encoding='utf-8').keys()
+            data = pd.read_excel(file_name, encoding='utf-8')
+        json_data = data.to_json(orient='records')
         return json.loads(json_data)
+
 
 if __name__ == '__main__':
 
-    csv_file_name = 'C:/Users/coolpad/Downloads/BusinessReport_2019-10-19_US.csv'
-    file_date = csv_file_name.split('/')[-1].split('_')[1]
-    file_mkp = csv_file_name.split('/')[-1].split('_')[2].split('.')[0]
+    tb_name = 'AscAsinBussiness'
+    file_path = '/home/data/bussiness/'
 
-    ets = ExcelToSql()
-    resp_data = ets.csv_to_json(csv_file_name)
-    ets.add_to_sql(file_date, file_mkp, resp_data)
+    files = os.listdir(file_path)
+    if files:
+        log.info('Starting add file to sql... | FileCount: %s' % len(files))
+        for f in files:
+            f_name = file_path + f
+            f_date = f.split('_')[0]
+            f_country = f.split('_')[1].split('.')[0]
+            ets = ExcelToSql()
+            resp_data = ets.data_to_json(f_name)
+            try:
+                ets.add_to_sql(tb_name, resp_data, snap_date=f_date, country=f_country)
+                log.info('Add %s to sql success, moving file...' % f_name)
+                # shutil.move(f_name, dst_path + f)
+            except Exception as e:
+                log.error('Add to sql error: %s' % e)
+        log.info('Add file to sql success!')
+    else:
+        log.info('Now is no files.')
