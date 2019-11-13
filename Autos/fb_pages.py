a1b148fc1bae94af1f1cf023cd310ec352cce434
luomurong <coolpad@yulong.com>
2019-11-13 10:30:39 +0800

    Update facebook scrapy for logs

--- Autos/fb_pages.py
+++ Autos/fb_pages.py
@@ -1,9 +1,26 @@
 # coding: utf-8
 import time
+import datetime
+import logging
 import random
 import requests
 from bs4 import BeautifulSoup
 
+
+formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+file_name = '/home/develop/logs/fb_logs/{}.log'.format(datetime.date.today())
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+
+fh = logging.FileHandler(file_name, mode='a+')
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+log.addHandler(fh)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+ch.setFormatter(formatter)
+log.addHandler(ch)
 
 class FbPages:
 
@@ -43,8 +60,8 @@ if __name__ == '__main__':
     ]
 
     fp = FbPages()
-    for i in range(3, 3000):
-        print(url.format(i + 1))
+    for i in range(7, 3000):
+        log.info(url.format(i + 1))
         proxies = {
             'http': 'http://{}'.format(random.choice(ip_ports))
         }
@@ -60,11 +77,13 @@ if __name__ == '__main__':
             pg_soup = BeautifulSoup(pg_resp.text, 'lxml')
             pg_likes = fp.get_likes(pg_soup)
             time.sleep(10)
+            # 解析About页面
             pgab_resp = requests.get(pg_link + 'about/?ref=page_internal', headers=headers, proxies=proxies)
             pgab_soup = BeautifulSoup(pgab_resp.text, 'lxml')
             pgab_terms = fp.get_terms(pgab_soup)
             pg_re = (pg_link, pg_name, pg_likes, pgab_terms)
-            print(pg_re)
+            log.info(pg_re)
+            # 写入文件
             with open('../fp_csv.csv', 'a', encoding='utf-8') as f:
                 f.write(str(pg_re) + '\n')
             time.sleep(10)
